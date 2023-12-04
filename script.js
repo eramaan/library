19bcf836e6f940544e4e432ce5f776752bd11f3d
eramaan <8112194+eramaan@users.noreply.github.com>
2023-12-04 20:02:24 +0100

    added more js, but still to work on it

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function Book(title, author, pages, read) {
 }
 
 let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-let eragon = new Book("Eragon", "C. Paolini", 684, true);
+let eragon = new Book("Eragon", "C. Paolini", 684, false);
 let harryPotter = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 320, true);
-let theLionTheWitchAndTheWardrobe = new Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 206, true);
+let theLionTheWitchAndTheWardrobe = new Book("The Lion, the Witch and the Wardrobe", "C.S. Lewis", 206, false);
 let aGameOfThrones = new Book("A Game of Thrones", "George R.R. Martin", 694, true);
 
 myLibrary.push(theHobbit);
@@ -23,8 +23,12 @@ myLibrary.push(aGameOfThrones);
 console.log(myLibrary);
 
 //  display the books
-function displayLibrary(title, author, pages, read) {
-    document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + author + "</p><p class=\"title\">"+ title + "</p><p class=\"pages\">" + pages + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility_off</span></button><button><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+function displayLibrary(item) {
+    if (item.read === true) {
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + item.author + "</p><p class=\"title\">" + item.title + "</p><p class=\"pages\">" + item.pages + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+    } else {    
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + item.author + "</p><p class=\"title\">" + item.title + "</p><p class=\"pages\">" + item.pages + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility_off</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+    }
 }
 
 myLibrary.forEach(displayLibrary);
@@ -35,11 +39,15 @@ let authorField = "b";
 let pagesField = "c";
 let readField = false; 
 
-//  access the form button
-let submitButton = document.querySelector("#submit-btn")
+//  access the buttons
+let submitButton = document.querySelector("#submit-btn");
 submitButton.addEventListener("click", addBookToLibrary);
 
+let deleteButtons = document.querySelectorAll(".delete");
+deleteButtons.forEach((deleteButton) => deleteButton.addEventListener("click", deleteBook));
 
+
+// functions
 function addBookToLibrary() {
     console.log("clicked");
     titleField = document.querySelector("#title").value;
@@ -61,7 +69,15 @@ function addBookToLibrary() {
 }
 
 function updateLibrary() {
-        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + authorField + "</p><p class=\"title\">"+ titleField + "</p><p class=\"pages\">" + pagesField + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility_off</span></button><button><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+    if (readField === true) {
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + authorField + "</p><p class=\"title\">"+ titleField + "</p><p class=\"pages\">" + pagesField + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+    } else {
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + authorField + "</p><p class=\"title\">"+ titleField + "</p><p class=\"pages\">" + pagesField + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility_off</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
     }
+}
 
 
+function deleteBook() {
+    console.log("click");
+    document.querySelector(".book").remove();
+}
