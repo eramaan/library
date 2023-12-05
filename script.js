4c3ef84f9b14155b53a77c5fd4074dd0f84f6e23
eramaan <8112194+eramaan@users.noreply.github.com>
2023-12-05 22:32:00 +0100

    added all the working for the buttons - ngl chat helped quite a lot this time

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ myLibrary.push(aGameOfThrones);
 console.log(myLibrary);
 
 //  display the books
-function displayLibrary(item) {
+function displayLibrary(item, index) {
     if (item.read === true) {
-        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + item.author + "</p><p class=\"title\">" + item.title + "</p><p class=\"pages\">" + item.pages + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\" data-index-number=\"" + index + "\"><p class=\"author\">" + item.author + "</p><p class=\"title\">" + item.title + "</p><p class=\"pages\">" + item.pages + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined read\">visibility</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
     } else {    
-        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + item.author + "</p><p class=\"title\">" + item.title + "</p><p class=\"pages\">" + item.pages + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility_off</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\" data-index-number=\"" + index + "\"><p class=\"author\">" + item.author + "</p><p class=\"title\">" + item.title + "</p><p class=\"pages\">" + item.pages + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined read\">visibility_off</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
     }
 }
 
@@ -42,9 +42,6 @@ let readField = false;
 //  access the buttons
 let submitButton = document.querySelector("#submit-btn");
 submitButton.addEventListener("click", addBookToLibrary);
-
-let deleteButtons = document.querySelectorAll(".delete");
-deleteButtons.forEach((deleteButton) => deleteButton.addEventListener("click", deleteBook));
 
 
 // functions
@@ -69,15 +66,83 @@ function addBookToLibrary() {
 }
 
 function updateLibrary() {
+    const lastIndex = myLibrary.length - 1; 
     if (readField === true) {
-        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + authorField + "</p><p class=\"title\">"+ titleField + "</p><p class=\"pages\">" + pagesField + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\" data-index-number=\"" + lastIndex + "\"><p class=\"author\">" + authorField + "</p><p class=\"title\">"+ titleField + "</p><p class=\"pages\">" + pagesField + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined read\">visibility</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
     } else {
-        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\"><p class=\"author\">" + authorField + "</p><p class=\"title\">"+ titleField + "</p><p class=\"pages\">" + pagesField + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined\">visibility_off</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+        document.querySelector("#shelf").insertAdjacentHTML( "beforeend" , "<div class=\"book\" data-index-number=\"" + lastIndex + "\"><p class=\"author\">" + authorField + "</p><p class=\"title\">"+ titleField + "</p><p class=\"pages\">" + pagesField + "</p><div class=\"btn\"><button><span class=\"material-symbols-outlined read\">visibility_off</span></button><button class=\"delete\"><span class=\"material-symbols-outlined\">delete</span></button></div></div>");
+    }
+
+    //again thanks chat that told me how to add again the listener to the new created elements
+    let newDeleteButton = document.querySelector(`.book[data-index-number="${lastIndex}"] .delete`);
+    newDeleteButton.addEventListener("click", deleteBook);
+
+    let newReadButton = document.querySelector(`.book[data-index-number="${lastIndex}"] .read`);
+    newReadButton.addEventListener("click", readToggle);
+}
+
+
+let deleteButtons = document.querySelectorAll(".delete");
+deleteButtons.forEach((deleteButton) => deleteButton.addEventListener("click", deleteBook));
+
+// function deleteBook() {
+//     console.log("click");
+//     indexToDelete = document.querySelector(".book").getAttribute("data-index-number")
+//     myLibrary.splice(indexToDelete, 1);
+//     bookToDelete = document.querySelector(".book").getAttribute('data-index-number="' + indexToDelete + '"')
+//     bookToDelete.remove();
+//     myLibrary.forEach(displayLibrary);
+//     console.log(myLibrary);
+// }
+
+//thanks chat that gived me the rght code, mine what a little fcked up
+
+function deleteBook(event) {
+    const clickedButton = event.target;
+    const bookElement = clickedButton.closest(".book");
+
+    if (bookElement) {
+        const indexToDelete = bookElement.getAttribute("data-index-number");
+
+        if (indexToDelete !== null) {
+            // Remove the book from the array
+            myLibrary.splice(indexToDelete, 1);
+
+            // Remove the book from the UI
+            bookElement.remove();
+
+            // Re-index the remaining books in the UI
+            myLibrary.forEach((book, index) => {
+                const bookElement = document.querySelector(`.book[data-index-number="${index}"]`);
+                if (bookElement) {
+                    bookElement.setAttribute("data-index-number", index);
+                }
+            });
+
+            console.log(myLibrary);
+        }
     }
 }
 
+let readButtons = document.querySelectorAll(".read");
+readButtons.forEach((readButton) => readButton.addEventListener("click", readToggle));
 
-function deleteBook() {
-    console.log("click");
-    document.querySelector(".book").remove();
-}
+function readToggle(event) {
+    console.log("readclick")
+    const clickedButton = event.target;
+    const bookElement = clickedButton.closest(".book");
+    const indexToToggle = bookElement.getAttribute("data-index-number");
+
+    myLibrary[indexToToggle].read = !myLibrary[indexToToggle].read;
+    
+    let readIcon = bookElement.querySelector(".read");
+
+    if (myLibrary[indexToToggle].read === true) {
+        readIcon.textContent = "visibility";
+    } else {
+        readIcon.textContent = "visibility_off";
+    }
+
+    console.log(indexToToggle);
+    console.log(myLibrary);
+    }
